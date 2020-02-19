f8b1d7482aff373414dbd794e933c9a1e94659aa
IN UH LEE <inuhlee@INui-MacBookPro.local>
2020-02-20 00:27:28 +0900

    page.evaluate사용하기(개꿀;;)

--- app.js
+++ app.js
@@ -16,17 +16,33 @@ const crawler = async () => {
         try {
           const page = await browser.newPage();
           await page.goto(record[1]);
-          const scoreEl = await page.$(".score.score_left .star_score");
-          // $는 태그를 찾는 메서드이다
-          if (scoreEl) {
-            const text = await page.evaluate(tag => tag.textContent, scoreEl);
-            // 찾은 태그의 텍스트를 뱉어낸다. 여기서 tag인자는 scoreEl을 의미한다
-            // 즉, evaluate의 두번째 인자는 $로 잡아낸 태그이고, 첫번째 인자는 그 태그를 처리하는 콜백 함수
+          const text = await page.evaluate(() => {
+            const score = document.querySelector(
+              ".score.score_left .star_score"
+            );
+            const score2 = document.querySelector(
+              ".score.score_left .star_score"
+            );
+            // window와 document는 evaluate메서드 안에서만 사용 가능하다
+            // https://try-puppeteer.appspot.com/
+            // 여기서 퍼페티어 코드를 실험해볼 수도 있다
+            // 이미지를 안 가져와서 크롤링 속도를 높일 수도 있다;;
+            // 심지어 크롤링한 페이지를 스크린샷으로 저장하거나 pdf로도 저장 가능함;;
+
+            if (score) {
+              // return {
+              //   score: score.textContent,
+              //   score2: score2.textContent
+              // };
+              // 결국 이 return값이 상단에 선언한 text변수의 값이 되는 것!!
+              return score.textContent;
+            }
+          });
+          if (result) {
             console.log(record[0], "평점", text.trim());
-            result[index] = [record[0], record[1], text.trim()];
-            // result자체가 배열이기 때문에 이렇게 넣어주면 2차원 배열이 된다
-            // 또한 인덱스를 지정해서 저장해주므로 이러면 for of처럼 순서도 보장된다.
+            result[i] = [record[0], record[1], text.trim()];
           }
+
           await page.close();
           //페이지를 안 닫아주면 에러가 날 수도 있음
           const str = stringify(result);
