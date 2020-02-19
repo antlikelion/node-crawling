e0603b50e89c6a33c3316e0eb6d6d00fcc672eb4
IN UH LEE <inuhlee@INui-MacBookPro.local>
2020-02-19 23:33:21 +0900

    첫 퍼페티어 크롤링

--- app.js
+++ app.js
@@ -2,42 +2,35 @@ const parse = require("csv-parse/lib/sync");
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 
-// const csv = fs.readFileSync("csv/data.csv");
-// const records = parse(csv.toString("utf-8"));
+const csv = fs.readFileSync("csv/data.csv");
+const records = parse(csv.toString("utf-8"));
 
 const crawler = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  //headless를 true로 설정하면, 브라우저를 뒷단에서 실행함
-  //어차피 크롤러는 서버(백엔드 에플리케이션)에서 돌아가는 것이기 때문
-  //(이 때는 콘솔을 찍어주던가 해서 크롤러가 실행되고 있다는 것을 확인해주자)
-  //개발 단계에서만 false로 설정하여 직접 돌아가는 것을 확인하는 것
-  // (config에서 관리해주면 될 듯)
-  //참고로 headless값을 설정하지 않으면 디폴트 값이 true이다.
-  //await은 프로미스가 resolve되기를 기다리는 것!
+  try {
+    const browser = await puppeteer.launch({ headless: false });
 
-  //그런데 애초에 탭을 여러개 연다는 것 자체가 여러 작업을 동시에 하기 위함인데
-  //순차대로 실행하는 것은 매우 비효율적이다.
-  // 그래서 아래와 같이 프로미스 올을 활용해준다
-  const [page, page2, page3] = await Promise.all([
-    browser.newPage(),
-    browser.newPage(),
-    browser.newPage()
-  ]);
-  await Promise.all([
-    page.goto("https://zerocho.com"),
-    page2.goto("https://naver.com"),
-    page3.goto("https://google.com")
-  ]);
-  await Promise.all([
-    page.waitFor(3000),
-    page2.waitFor(1000),
-    page3.waitFor(2000)
-  ]);
-  // 끄는 작업은 그다지 시간이 걸리지 않기 때문에 굳이 프로미스 올을 써줄 이유가 없다
-  await page.close();
-  await page2.close();
-  await page3.close();
-  await browser.close();
-  // 13.브라우저를 끈다(메모리 관리를 위함)
+    await Promise.all(
+      records.map(async (record, index) => {
+        try {
+          const page = await browser.newPage();
+          await page.goto(record[1]);
+          const scoreEl = await page.$(".score.score_left .star_score");
+          // $는 태그를 찾는 메서드이다
+          if (scoreEl) {
+            const text = await page.evaluate(tag => tag.textContent, scoreEl);
+            // 찾은 태그의 텍스트를 뱉어낸다. 여기서 tag인자는 scoreEl을 의미한다
+            // 즉, evaluate의 두번째 인자는 $로 잡아낸 태그이고, 첫번째 인자는 그 태그를 처리하는 콜백 함수
+            console.log(record[0], "평점", text.trim());
+          }
+          await page.close();
+          //페이지를 안 닫아주면 에러가 날 수도 있음
+        } catch (crawlError) {
+          console.error(crawlError);
+        }
+      })
+    );
+  } catch (error) {
+    console.error(error);
+  }
 };
 crawler();
