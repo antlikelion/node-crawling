f4d33e5c87b74de4687d949cd166355c1bd96f83
IN UH LEE <inuhlee@INui-MacBookPro.local>
2020-02-19 21:18:43 +0900

    Promise.all과 for of문의 차이

--- app.js
+++ app.js
@@ -23,20 +23,30 @@ for (const [index, record] of records.entries()) {
   // 자바스크립트 객체.entries()를 쓰면 내부 배열이 [key, value]모양 배열로 바뀐다
   console.log(index, record);
 }
+// for of문은 await과 함께 쓰면 순서가 보장된다
 
 const crawler = async () => {
-  await Promise.all(
-    records.map(async element => {
-      const response = await axios.get(element.링크);
-      //페이지(html)를 요청하는 코드
-      if (response.status === 200) {
-        const html = response.data;
-        console.log(html);
-        const $ = cheerio.load(html);
-        const text = $(".score.score_left .star_score").text();
-        console.log(element.제목, "평점", text.trim());
-      }
-    })
-  );
+  for (const [index, element] of records.entries()) {
+    const response = await axios.get(element.링크);
+    if (response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const text = $(".score.score_left .star_score").text();
+      //.text()가 태그는 무시하고 텍스트만 긁어옴
+      console.log(element.제목, "평점~", text.trim());
+    }
+  }
+  // await Promise.all(
+  //   records.map(async element => {
+  //     const response = await axios.get(element.링크);
+  //     //페이지(html)를 요청하는 코드
+  //     if (response.status === 200) {
+  //       const html = response.data;
+  //       const $ = cheerio.load(html);
+  //       const text = $(".score.score_left .star_score").text();
+  //       console.log(element.제목, "평점", text.trim());
+  //     }
+  //   })
+  // );
 };
 crawler();
